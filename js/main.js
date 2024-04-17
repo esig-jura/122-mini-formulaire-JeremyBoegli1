53891e5c2bea7ca354efe78ae702fda087b118ab
jeremy.boegli <jeremy.boegli@divcom.ch>
2024-04-17 16:15:22 +0200

    Changement background

--- js/main.js
+++ js/main.js
@@ -9,22 +9,51 @@
 const formulaire = document.querySelector('form');
 const txtMDP = document.querySelector('#mdp');
 const txtMDPConfirm = document.querySelector('#mdpc');
-console.log(formulaire, txtMDP, txtMDPConfirm);
+const listeBackground = document.querySelector('#background');
+const body = document.querySelector('body');
+console.log(body);
 
-// Ecouter l'envoi du formulaire
-formulaire.addEventListener('submit', function coucou(event) {
+// Ecouter l'envoi du formulaire (=> c'est pour être quitte d'écrire function
+formulaire.addEventListener('submit', (event) => {
     //Stoppe l'envoi du formulaire
     event.preventDefault();
+    //Tableau des erreurs
+    let erreurs = [];
     // Récupérer la valeur du mdp et de la confirmation du mdp
     let mdp = txtMDP.value;
     let mdpConfirm = txtMDPConfirm;
+
+//Tester si mdp > 3
+if(txtMDP.value.length < 3){
+    erreurs.push('Le mot de passe doit contenir au moins 3 caractères !')
+}
+
 
 //Tester si les mdp sont identiques
     if(mdp !== mdpConfirm) {
         alert('Les mots de passes sont différents !!')
     } else {
-        alert('Compte créé avec succès');
+        alert('Compte créé avec succès !');
         //Envoyer le formulaire
         formulaire.submit();
     }
+
+    //S'il y a des erreurs
+    if(erreurs.length > 0) {
+        let msg = '';
+        //Pour chaque erreur dans le tableau
+        for (let err of erreurs){
+            //Créé une nouvelle ligne (\n) pour chaque erreur
+            msg += '\n - ' + err;
+        }
+    }
+    alert(msg);
+    //Stop la fonction
+    return;
 });
+
+//Ecouter le changement de valeur dans la liste déroulante
+listeBackground.addEventListener('change', () => {
+    //Remplacer image de fond du body
+    body.style.backgroundImage = 'url(./img/backgrounds/' + listeBackground.value + ')';
+})
